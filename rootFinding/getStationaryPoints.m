3e757b636d4524e5ab140137888e20aaac1b985a
Unknown <andrewgibbs1987@gmail.com>
2018-02-19 14:46:18 +0100

    Made it so that analytic phases can find their stationary points much faster

--- rootFinding/getStationaryPoints.m
+++ rootFinding/getStationaryPoints.m
@@ -1,26 +1,29 @@
 function [gStationaryPoints, gSingularities, gSPorders,gPoleOrders] = getStationaryPoints(a,b,rectRad,...
                                                                      analytic, G, RectTol, N , visuals)
     %This one should work better, although the '2' is quite arbitrary.
+    Nrect=15;
     if isempty(rectRad)
         rectRad=.75*(b-a);
     end
-    initRect=[a-rectRad-rectRad*1i  b+rectRad-rectRad*1i  b+rectRad+rectRad*1i  a-rectRad+rectRad*1i];
-   
+    initRect=[a-rectRad-rectRad*1i  b+rectRad-rectRad*1i  b+rectRad+rectRad*1i  a-rectRad+rectRad*1i];  
 
-    if ~analytic
-       error('Can only detect stationary points of analytic phase functions'); 
+    if analytic %can do a quicker verison of the zeros search:
+        [gStationaryPoints, gSPorders] = findZerosRect( G{2}, G{3}, initRect, RectTol, Nrect, visuals );
+        gSingularities=[];
+        gPoleOrders=[];
+    else %need ot zoom in further into the rectangle:
+        %now find all stationary points inside of this rectangle
+        [gStationaryPoints, gSingularities, gSPorders, gPoleOrders] = findZerosSingsRect( G{2}, G{3}, initRect, RectTol, Nrect , visuals);        
     end
     %assumes g is a cell array containing increasing derivatives of phase
     %function g. Requires at least up to G{3}:=g"(x)
     if length(G)<3
         if ~analytic
             error('Require up to second derivative of phase non-analytic functions, OR an anlytic phase function, to automatically detect stationary points');
-        else
-            G=finishDerivs( G, 3, N, RectTol );
+%Not going to bother adding extra derivatives via Cauchy differentiation,
+%it's too unstable.
+            %         else
+%             G=finishDerivs( G, 3, N, RectTol );
         end
     end
-
-    %now find all stationary points inside of this rectangle
-    [gStationaryPoints, gSingularities, gSPorders, gPoleOrders] = findZerosSingsRect( G{2}, G{3}, initRect, RectTol, N , visuals);
 end
-
