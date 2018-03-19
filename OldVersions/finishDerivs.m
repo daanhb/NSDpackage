2ce9848c50c293bfc890dd513a7e005379a559ec
Unknown <andrewgibbs1987@gmail.com>
2018-03-19 14:27:53 +0100

    did this stuff get ignored or what

--- OldVersions/finishDerivs.m
+++
@@ -1,9 +0,0 @@
-function G = finishDerivs( G, newLength, Npts, zeroThresh )
-%newLength=(max(pathPowers)+1)
-%zeroThresh=rectTol
-    origGlength=length(G);
-    for extraG=(origGlength+1) : newLength
-        G{extraG}=@(z) arrayfun(@(Z) CauchyDiff( Z,G{origGlength}, Z, zeroThresh, extraG-origGlength, Npts ), z);
-    end
-
-end
